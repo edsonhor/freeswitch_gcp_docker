f6c462797e0dbe84602f7fa87e515ac3de70fe70
Edson H Philippe <edson.philippe@ufl.edu>
2018-03-27 03:13:18 +0000

    compile from source code

--- Dockerfile
+++ Dockerfile
@@ -10,13 +10,41 @@ ENV DEBIAN_FRONTEND noninteractive
 # - xmlstarlet for XML manipulation via ENTRYPOINT
 # - git for overly configuration from a git repository
 # - vim for dev/test editing in the fs container
+# - docker run -it --net=host --name freeswitch freeswitch
 
-RUN apt-get update && apt-get -y install curl && \
+RUN apt-get update && apt-get -y install curl wget && \
       curl https://files.freeswitch.org/repo/deb/debian/freeswitch_archive_g0.pub | apt-key add - && \
       echo "deb http://files.freeswitch.org/repo/deb/freeswitch-1.6/ jessie main" > /etc/apt/sources.list.d/freeswitch.list && \
       apt-get -y update && apt-get -y upgrade && \
-      apt-get -y install xmlstarlet git vim freeswitch-all freeswitch-all-dbg gdb && \
-      rm -rf /var/lib/apt/lists/*
+      apt-get -y install xmlstarlet git vim 
+
+
+
+# manually get the code and compile it
+
+RUN wget -O - https://files.freeswitch.org/repo/deb/debian/freeswitch_archive_g0.pub | apt-key add - && \
+echo "deb http://files.freeswitch.org/repo/deb/freeswitch-1.6/ jessie main" > /etc/apt/sources.list.d/freeswitch.list && \
+apt-get update && \
+apt-get install -y --force-yes freeswitch-video-deps-most && \
+#freeswitch-all freeswitch-all-dbg gdb && \
+      rm -rf /var/lib/apt/lists/* && \
+
+# then let's get the source. Use the -b flag to get a specific branch
+cd /etc/  && \
+git clone https://freeswitch.org/stash/scm/fs/freeswitch.git -bv1.6 freeswitch && \
+cd freeswitch && \
+ 
+# Because we're in a branch that will go through many rebases, it's
+# better to set this one, or you'll get CONFLICTS when pulling (update).
+git config pull.rebase true && \
+ 
+# ... and do the build
+./bootstrap.sh -j && \
+./configure && \
+make && \
+make install 
+
+   
 
 VOLUME /etc/freeswitch
 VOLUME /var/lib/freeswitch
@@ -27,6 +55,8 @@ EXPOSE 5060/tcp 5060/udp 5080/tcp 5080/udp
 EXPOSE 5066/tcp 7443/tcp
 EXPOSE 8021/tcp
 
+WORKDIR /usr/local/freeswitch/bin
+
 # RTP port range
 EXPOSE 16384-32768/udp
 
@@ -35,4 +65,4 @@ COPY reload.sh /opt/local/bin/reload.sh
 
 ENTRYPOINT ["/opt/local/bin/entry.sh"]
 
-CMD ["freeswitch", "-c", "-nf"]
+CMD ["/usr/local/freeswitch/bin/freeswitch", "-c", "-nf"]
--- entry.sh
+++ entry.sh
@@ -7,13 +7,13 @@ echo 'Updating default_password'
 xmlstarlet ed -L -u \
   "/include/X-PRE-PROCESS[@data = 'default_password=1234']/@data" \
   -v "default_password=$DEFAULT_PASSWORD" \
-    /etc/freeswitch/vars.xml
+    /usr/local/freeswitch/conf/vars.xml
 
 # timerfd support
 # https://wiki.freeswitch.org/wiki/Mod_timerfd
 if [ "$SOFTTIMER_TIMERFD" = 'true' ]; then
   echo 'Enabling softtimer-timerfd'
-  sed -i 's%<!-- <param name="enable-softtimer-timerfd" value="true"/> -->%<param name="enable-softtimer-timerfd" value="true"/>%g' /etc/freeswitch/autoload_configs/switch.conf.xml
+  sed -i 's%<!-- <param name="enable-softtimer-timerfd" value="true"/> -->%<param name="enable-softtimer-timerfd" value="true"/>%g' /usr/local/freeswitch/conf/autoload_configs/switch.conf.xml
 fi
 
 
@@ -24,12 +24,12 @@ if [ "$GCP" = 'true' ]; then
   
   # vars.xml modification
   gcp_external_ip=$(curl --header "Metadata-Flavor : Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
-  sed -i 's@<X-PRE-PROCESS cmd="set" data="domain=$${local_ip_v4}"/>@<X-PRE-PROCESS cmd="set" data="domain='"$gcp_external_ip"'"/>@' /etc/freeswitch/vars.xml
+  sed -i 's@<X-PRE-PROCESS cmd="set" data="domain=$${local_ip_v4}"/>@<X-PRE-PROCESS cmd="set" data="domain='"$gcp_external_ip"'"/>@' /usr/local/freeswitch/conf/vars.xml
 
   # sip_profiles/internal.xml modifications
    
-   sed -i 's@<param name="ext-rtp-ip" value="auto-nat"/>@<param name="ext-rtp-ip" value="autonat:'"$gcp_external_ip"'"/>@' /etc/freeswitch/sip_profiles/internal.xml
-   sed -i 's@<param name="ext-sip-ip" value="auto-nat"/>@<param name="ext-sip-ip" value="autonat:'"$gcp_external_ip"'"/>@' /etc/freeswitch/sip_profiles/internal.xml   
+   sed -i 's@<param name="ext-rtp-ip" value="auto-nat"/>@<param name="ext-rtp-ip" value="autonat:'"$gcp_external_ip"'"/>@' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+   sed -i 's@<param name="ext-sip-ip" value="auto-nat"/>@<param name="ext-sip-ip" value="autonat:'"$gcp_external_ip"'"/>@' /usr/local/freeswitch/conf/sip_profiles/internal.xml   
 
 fi
 
@@ -47,54 +47,54 @@ if [ "$EC2" = 'true' ]; then
   xmlstarlet ed -L -u \
     "/include/X-PRE-PROCESS[@data = 'bind_server_ip=auto']/@cmd" \
     -v "exec-set" \
-      /etc/freeswitch/vars.xml
+      /usr/local/freeswitch/conf/vars.xml
   xmlstarlet ed -L -u \
     "/include/X-PRE-PROCESS[@data = 'bind_server_ip=auto']/@data" \
     -v "bind_server_ip=curl -s http://instance-data/latest/meta-data/public-ipv4" \
-      /etc/freeswitch/vars.xml
+      /usr/local/freeswitch/conf/vars.xml
 
   # external_rtp_ip
   xmlstarlet ed -L -u \
     "/include/X-PRE-PROCESS[@data = 'external_rtp_ip=stun:stun.freeswitch.org']/@cmd" \
     -v "exec-set" \
-      /etc/freeswitch/vars.xml
+      /usr/local/freeswitch/conf/vars.xml
   xmlstarlet ed -L -u \
     "/include/X-PRE-PROCESS[@data = 'external_rtp_ip=stun:stun.freeswitch.org']/@data" \
     -v "external_rtp_ip=curl -s http://instance-data/latest/meta-data/public-ipv4" \
-      /etc/freeswitch/vars.xml
+      /usr/local/freeswitch/conf/vars.xml
 
   # external_sip_ip
   xmlstarlet ed -L -u \
     "/include/X-PRE-PROCESS[@data = 'external_sip_ip=stun:stun.freeswitch.org']/@cmd" \
     -v "exec-set" \
-      /etc/freeswitch/vars.xml
+      /usr/local/freeswitch/conf/vars.xml
   xmlstarlet ed -L -u \
     "/include/X-PRE-PROCESS[@data = 'external_sip_ip=stun:stun.freeswitch.org']/@data" \
     -v "external_sip_ip=curl -s http://instance-data/latest/meta-data/public-ipv4" \
-      /etc/freeswitch/vars.xml
+      /usr/local/freeswitch/conf/vars.xml
 
   # sip_profiles/internal.xml modifications
-  sed -i 's%<!--<param name="aggressive-nat-detection" value="true"/>-->%<param name="aggressive-nat-detection" value="true"/>%g' /etc/freeswitch/sip_profiles/internal.xml
-  sed -i 's%<!--<param name="multiple-registrations" value="contact"/>-->%<param name="multiple-registrations" value="true"/>%g' /etc/freeswitch/sip_profiles/internal.xml
-  sed -i 's%<param name="ext-rtp-ip" value="auto-nat"/>%<param name="ext-rtp-ip" value="$${external_rtp_ip}"/>%g' /etc/freeswitch/sip_profiles/internal.xml
-  sed -i 's%<param name="ext-sip-ip" value="auto-nat"/>%<param name="ext-sip-ip" value="$${external_sip_ip}"/>%g' /etc/freeswitch/sip_profiles/internal.xml
-  sed -i 's%<!--<param name="NDLB-received-in-nat-reg-contact" value="true"/>-->%<param name="NDLB-received-in-nat-reg-contact" value="true"/>%g' /etc/freeswitch/sip_profiles/internal.xml
-  sed -i 's%<!--<param name="NDLB-force-rport" value="true"/>-->%<param name="NDLB-force-rport" value="true"/>%g' /etc/freeswitch/sip_profiles/internal.xml
-  sed -i 's%<!--<param name="NDLB-broken-auth-hash" value="true"/>-->%<param name="NDLB-broken-auth-hash" value="true"/>%g' /etc/freeswitch/sip_profiles/internal.xml
-  sed -i 's%<!--<param name="enable-timer" value="false"/>-->%<param name="enable-timer" value="false"/>%g' /etc/freeswitch/sip_profiles/internal.xml
-  sed -i 's%<param name="auth-calls" value="$${internal_auth_calls}"/>%<param name="auth-calls" value="true"/>%g' /etc/freeswitch/sip_profiles/internal.xml
+  sed -i 's%<!--<param name="aggressive-nat-detection" value="true"/>-->%<param name="aggressive-nat-detection" value="true"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+  sed -i 's%<!--<param name="multiple-registrations" value="contact"/>-->%<param name="multiple-registrations" value="true"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+  sed -i 's%<param name="ext-rtp-ip" value="auto-nat"/>%<param name="ext-rtp-ip" value="$${external_rtp_ip}"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+  sed -i 's%<param name="ext-sip-ip" value="auto-nat"/>%<param name="ext-sip-ip" value="$${external_sip_ip}"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+  sed -i 's%<!--<param name="NDLB-received-in-nat-reg-contact" value="true"/>-->%<param name="NDLB-received-in-nat-reg-contact" value="true"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+  sed -i 's%<!--<param name="NDLB-force-rport" value="true"/>-->%<param name="NDLB-force-rport" value="true"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+  sed -i 's%<!--<param name="NDLB-broken-auth-hash" value="true"/>-->%<param name="NDLB-broken-auth-hash" value="true"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+  sed -i 's%<!--<param name="enable-timer" value="false"/>-->%<param name="enable-timer" value="false"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
+  sed -i 's%<param name="auth-calls" value="$${internal_auth_calls}"/>%<param name="auth-calls" value="true"/>%g' /usr/local/freeswitch/conf/sip_profiles/internal.xml
 
   # sip_profiles/external.xml
-  sed -i 's%<!--<param name="aggressive-nat-detection" value="true"/>-->%<param name="aggressive-nat-detection" value="true"/>%g' /etc/freeswitch/sip_profiles/external.xml
-  sed -i 's%<param name="ext-rtp-ip" value="auto-nat"/>%<param name="ext-rtp-ip" value="$${external_rtp_ip}"/>%g' /etc/freeswitch/sip_profiles/external.xml
-  sed -i 's%<param name="ext-sip-ip" value="auto-nat"/>%<param name="ext-sip-ip" value="$${external_sip_ip}"/>%g' /etc/freeswitch/sip_profiles/external.xml
+  sed -i 's%<!--<param name="aggressive-nat-detection" value="true"/>-->%<param name="aggressive-nat-detection" value="true"/>%g' /usr/local/freeswitch/conf/sip_profiles/external.xml
+  sed -i 's%<param name="ext-rtp-ip" value="auto-nat"/>%<param name="ext-rtp-ip" value="$${external_rtp_ip}"/>%g' /usr/local/freeswitch/conf/sip_profiles/external.xml
+  sed -i 's%<param name="ext-sip-ip" value="auto-nat"/>%<param name="ext-sip-ip" value="$${external_sip_ip}"/>%g' /usr/local/freeswitch/conf/sip_profiles/external.xml
 
   # todo: add this
   # <param name="NDLB-force-rport" value="true"/>
 
   # autoload_configs/switch.conf.xml
-  sed -i 's%<!-- <param name="rtp-start-port" value="16384"/> -->%<param name="rtp-start-port" value="16384"/>%g' /etc/freeswitch/autoload_configs/switch.conf.xml
-  sed -i 's%<!-- <param name="rtp-end-port" value="32768"/> -->%<param name="rtp-end-port" value="32768"/>%g' /etc/freeswitch/autoload_configs/switch.conf.xml
+  sed -i 's%<!-- <param name="rtp-start-port" value="16384"/> -->%<param name="rtp-start-port" value="16384"/>%g' /usr/local/freeswitch/conf/autoload_configs/switch.conf.xml
+  sed -i 's%<!-- <param name="rtp-end-port" value="32768"/> -->%<param name="rtp-end-port" value="32768"/>%g' /usr/local/freeswitch/conf/autoload_configs/switch.conf.xml
 fi
 
 # overlay configuration
